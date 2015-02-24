119d2796e3ec094494ceae124856d20d0e8ce9c3
streeter <streeter@ebi.ac.uk>
2015-02-24 15:20:10 +0000

    development

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,7 +8,7 @@ controllers.controller('LineDetailCtrl', ['$scope', '$routeParams', 'esClient',
   function($scope, $routeParams, esClient) {
     $scope.data = esClient.getSource({
         index: 'hipsci',
-        type: 'cell_line',
+        type: 'cellLine',
         id: $routeParams.ipscName
     }).then(function(resp) {
         $scope.data = resp;
@@ -28,23 +28,45 @@ controllers.controller('DonorDetailCtrl', ['$scope', '$routeParams', 'esClient',
   }
 ]);
 
-controllers.controller('DonorListCtrl', ['$scope', '$routeParams', 'donorSearcher',
-  function($scope, $routeParams, esClient) {
-    $scope.page = 0;
+controllers.controller('DonorListCtrl', ['$scope', '$routeParams', 'itemSearcher',
+  function($scope, $routeParams, itemSearcher) {
+    $scope.searchParams = {
+        documentType: 'donor',
+        page: 0,
+        size: 10,
+        query: '',
+        fields: ['name', 'sex'],
+        columnHeaders: ['Name', 'Sex']
+    };
 
     $scope.search = function() {
-        donorSearcher.search($scope.page)
+        itemSearcher.search($scope.searchParams)
         .then(function(resp) {
-            $scope.data = resp;
+            $scope.data = resp.hits.hits;
+            for (var i=0; i<$scope.data.length; i++) {
+                $scope.data[i].columnHrefs = {};
+                for (var j=0; j<$scope.searchParams.fields.length; j++) {
+                    var field = $scope.searchParams.fields[j];
+                    if (field == 'name') {
+                        $scope.data[i].columnHrefs[field] = "#/donors/" + $scope.data[i].fields[field][0];
+                    }
+                }
+            }
         });
     };
 
+    $scope.exportData = function(format) {
+        itemSearcher.exportData($scope.searchParams, format);
+    };
+
     $scope.loadNext = function() {
-        $scope.search($scope.page++);
+        $scope.searchParams.page++;
+        $scope.search();
     };
 
     $scope.loadPrevious = function() {
-        $scope.search($scope.page--);
+        $scope.searchParams.page--;
+        $scope.search();
     };
 
     $scope.search();
--- app/js/services.js
+++ app/js/services.js
@@ -13,18 +13,49 @@ services.service('esClient', ['esFactory',
   }
 ]);
 
-services.factory('donorSearcher', ['esClient',
-  var search = function(offset) {
-    esClient.search({
-        index: 'hipsci',
-        type: 'donor',
-        body: {
-            fields: 'Name',
-            size: 10,
-            from: (offset || 0) * 10
-        }
-    });
-  };
-  return {search:search};
-]);
+services.factory('itemSearcher', ['esClient',
+  function(esClient) {
+      var buildSearchBody = function(searchParams) {
+          return {
+                fields: searchParams.fields,
+                size: searchParams.size,
+                from: searchParams.page * searchParams.size
+          };
+      };
+      var search = function(searchParams) {
+        return esClient.search({
+            index: 'hipsci',
+            type: searchParams.documentType,
+            body: buildSearchBody(searchParams)
+        });
+      };
+
+      var exportData = function(searchParams, format) {
+          var form = document.createElement('form');
+          var body = buildSearchBody(searchParams);
+          delete body.size;
+          delete body.from;
+          body.column_names = searchParams.columnHeaders;
+          form.action='http://127.0.0.1:3000/hipsci/' + searchParams.documentType + '/_search.' +format;
+          form.method='POST';
+          form.target="_self";
+
+          for (var key in body) {
+              if (body.hasOwnProperty(key)) {
+                  var input = document.createElement("textarea");
+                  input.setAttribute('type', 'hidden');
+                  input.setAttribute('name', key);
+                  input.value =  (body[key] instanceof Array || typeof body[key] === "object")
+                                ? JSON.stringify(body[key]) : body[key];
+                  form.appendChild(input);
+              }
+          }
+          form.style.display = 'none';
+          document.body.appendChild(form);
+          form.submit();
+      };
+
+
+      return {search: search, exportData: exportData};
+}]);
 
