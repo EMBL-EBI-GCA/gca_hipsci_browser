d3b4744c1afaadcc7b193f19ba8014bda289f2b3
streeter <streeter@ebi.ac.uk>
2015-04-06 21:36:32 +0100

    directive to watch for description lists

--- app/js/item_detail.js
+++ app/js/item_detail.js
@@ -2,6 +2,7 @@
 
 var itemDetail = angular.module('hipsciBrowser.itemDetail', []);
 
+/*
 itemDetail.directive('itemDetailDl', function() {
   return {
     restrict: 'A',
@@ -29,3 +30,40 @@ itemDetail.directive('itemDetailDl', function() {
     }
   };
 });
+*/
+
+itemDetail.directive('itemDetailDl', ['$window', function($window) {
+  return {
+    restrict: 'A',
+    scope: false,
+    compile: function(tElement) {
+        tElement.addClass('dl-horizontal');
+    return {
+    post: function(scope, iElement, iAttrs) {
+        var setProperties = function() {
+            var dtEls = iElement.find('dt');
+            var ddEls = iElement.find('dd');
+            console.log(dtEls.length);
+            console.log(ddEls.length);
+            if (dtEls.first().position().top === ddEls.first().position().top) {
+                var dtWidth = iElement.width() * 0.3;
+                dtEls.width(dtWidth);
+                ddEls.css('margin-left', dtWidth + 20 + 'px');
+            }
+            else {
+                dtEls.width(iElement.width());
+                ddEls.css('margin-left', 40 + 'px');
+            }
+        };
+        //setProperties();
+
+        var win = angular.element($window);
+        win.bind('resize', setProperties);
+
+        scope.$watch('data', setProperties);
+
+    }
+    };
+    }
+  };
+}]);
