a642bd8317a0cc79efa18594f2e6d83f439b88ab
streeter <streeter@ebi.ac.uk>
2015-10-10 23:05:36 +0100

    json and groovy for the assay type

---
+++ elasticsearch_settings/scripts/hipsci_assay_transform.groovy
@@ -0,0 +1,16 @@
+
+ctx._source.calculated = [:]
+
+ctx._source.calculated.access = ctx._source.file.openAccess ? 'Open access' : ctx._source.file.openAccess != null ? 'Managed access' : ''
+
+ctx._source.searchable = [:]
+ctx._source.searchable.fixed = []
+
+if (ctx._source.sample && ctx._source.sample.name) {
+    String name = ctx._source.sample.name
+    ctx._source.searchable.fixed << name
+    name = name.replaceFirst(/^HPSI[a-z0-9]*-/, '')
+    ctx._source.searchable.fixed << name
+    name = name.replaceFirst(/_[0-9]*$/, '')
+    ctx._source.searchable.fixed << name
+}
