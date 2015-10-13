cde41bcbb660d3723fc19fdd768ed35abb3bee44
streeter <streeter@ebi.ac.uk>
2015-10-13 16:24:18 +0100

    groovy script to work with multiple samples per file document

--- elasticsearch_settings/scripts/hipsci_file_transform.groovy
+++ elasticsearch_settings/scripts/hipsci_file_transform.groovy
@@ -6,11 +6,15 @@ ctx._source.calculated.access = ctx._source.archive.openAccess ? 'Open access' :
 ctx._source.searchable = [:]
 ctx._source.searchable.fixed = []
 
-if (ctx._source.sample && ctx._source.sample.name) {
-    String name = ctx._source.sample.name
-    ctx._source.searchable.fixed << name
-    name = name.replaceFirst(/^HPSI[a-z0-9]*-/, '')
-    ctx._source.searchable.fixed << name
-    name = name.replaceFirst(/_[0-9]*$/, '')
-    ctx._source.searchable.fixed << name
+if (ctx._source.samples) {
+    for (sample in ctx._source.samples) {
+        if (sample.name) {
+            String name = sample.name
+            ctx._source.searchable.fixed << name
+            name = name.replaceFirst(/^HPSI[a-z0-9]*-/, '')
+            ctx._source.searchable.fixed << name
+            name = name.replaceFirst(/_[0-9]*$/, '')
+            ctx._source.searchable.fixed << name
+        }
+    }
 }
