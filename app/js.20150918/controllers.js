422141de1217bd3a90b0694f1dad7ce5be44750d
streeter <streeter@ebi.ac.uk>
2015-10-13 15:23:55 +0100

    Allow for multiple samples in a file document

--- app/js.20150918/controllers.js
+++ app/js.20150918/controllers.js
@@ -294,10 +294,10 @@ controllers.controller('LineListCtrl', function() {
 controllers.controller('FileListCtrl', function() {
     var controller=this;
     this.documentType = 'file';
-    this.esFields = ['files.name', 'files.md5', 'files.url', 'assay.type', 'description', 'sample.name', 'sample.cellType',
-        'sample.bioSamplesAccession', 'archive.name', 'archive.accessionType',
+    this.esFields = ['files.name', 'files.md5', 'files.url', 'assay.type', 'description', 'samples.name', 'samples.cellType',
+        'samples.bioSamplesAccession', 'archive.name', 'archive.accessionType',
         'archive.accession', 'archive.url',
-        'assay.growingConditions', 'sample.diseaseStatus', 'sample.sex', 'assay.description'];
+        'assay.growingConditions', 'samples.diseaseStatus', 'samples.sex', 'assay.description'];
 
     this.columnHeadersMap = {
         'files.name': 'File name',
@@ -305,25 +305,25 @@ controllers.controller('FileListCtrl', function() {
         'files.url': 'File url',
         'assay.type': 'Assay',
         'description': 'Description',
-        'sample.name': 'Cell line',
-        'sample.cellType': 'Cell type',
-        'sample.bioSamplesAccession': 'Biosample',
+        'samples.name': 'Cell line',
+        'samples.cellType': 'Cell type',
+        'samples.bioSamplesAccession': 'Biosample',
         'archive.name': 'Archive',
         'archive.accessionType': 'Accession type',
         'archive.accession': 'Accession',
         'archive.url': 'Archive url',
         'assay.growingConditions': 'Cell line growing conditions for this assay',
-        'sample.diseaseStatus': 'Disease status',
-        'sample.sex': 'Sex',
+        'samples.diseaseStatus': 'Disease status',
+        'samples.sex': 'Sex',
         'assay.description': 'Assay description',
     };
 
     this.htmlFields = [
-        'sample.name', 'assay.type', 'description', 'file', 'archive'
+        'samples.name', 'assay.type', 'description', 'file', 'archive'
     ];
 
     this.htmlHeadersMap = {
-        'sample.name': 'Cell line',
+        'samples.name': 'Cell line',
         'assay.type': 'Assay',
         'description': 'Description',
         'file': 'File download',
@@ -347,7 +347,7 @@ controllers.controller('FileListCtrl', function() {
             var field = controller.htmlFields[i];
             var hitStr = 'hit['+i+']';
             trChildren.push(
-              field == 'sample.name' ? '<td class="name"><a ng-if="'+hitStr+'.isIPS" ng-href="#/lines/{{'+hitStr+'.name}}" ng-bind="'+hitStr+'.name"></a><span ng-if="!'+hitStr+'.isIPS" ng-bind="'+hitStr+'.name" ></span></td>'
+              field == 'samples.name' ? '<td class="name"><div ng-repeat="sample in '+hitStr+'"><a ng-if="sample.isIPS" ng-href="#/lines/{{sample.name}}" ng-bind="sample.name"></a><span ng-if="!sample.isIPS" ng-bind="sample.name" ></span></div></td>'
               : field == 'file' ? '<td class="name"><span ng-repeat="url in '+hitStr+'"><a ng-href="{{url}}"><span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span></a></span></td>'
               : field == 'archive' ? '<td class="name"><a ng-href="{{'+hitStr+'.url}}" ng-bind="'+hitStr+'.name"></a></td>'
               : '<td ng-bind="'+hitStr+'"></td>'
@@ -358,7 +358,7 @@ controllers.controller('FileListCtrl', function() {
 
     this.processHitFields = function(hitFields, fields) {
         var processedFields = [];
-        processedFields[0] = { name: hitFields['sample.name'][0], isIPS: hitFields['sample.cellType'][0] == 'iPSC' ? true : false};
+        processedFields[0] = [];
         processedFields[1] = hitFields['assay.type'][0];
         processedFields[2] = hitFields.description[0];
         processedFields[3] = hitFields['files.url'];
@@ -366,7 +366,10 @@ controllers.controller('FileListCtrl', function() {
             name: hitFields.hasOwnProperty('archive.name') ? hitFields['archive.name'][0] : undefined,
             url: hitFields.hasOwnProperty('archive.url') ? hitFields['archive.url'][0] : undefined,
         };
-        console.log(processedFields);
+
+        for (var i=0; i<hitFields['samples.name'].length; i++) {
+            processedFields[0].push({ name: hitFields['samples.name'][i], isIPS: hitFields['samples.cellType'][i] == 'iPSC' ? true : false});
+        }
         return processedFields;
     };
 
