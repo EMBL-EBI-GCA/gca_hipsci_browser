5546a753a0e2f926c207355f4ee3573f5829de91
streeter <streeter@ebi.ac.uk>
2015-10-30 12:45:25 +0000

    some settings to allow searching on assay type

--- elasticsearch_settings/scripts/hipsci_cellLine_transform.groovy
+++ elasticsearch_settings/scripts/hipsci_cellLine_transform.groovy
@@ -1,17 +1,41 @@
 
 ctx._source.calculated = [:]
+ctx._source.searchable = [:]
+ctx._source.searchable.fixed = []
+ctx._source.searchable.free = []
 
 ctx._source.calculated.assayCount = ctx._source.assays ? ctx._source.assays.size() : 0
 
 ctx._source.calculated.assays = []
 if (ctx._source.assays) {
-  if (ctx._source.assays.gtarray) ctx._source.calculated.assays << 'Genotyping array'
-  if (ctx._source.assays.gexarray) ctx._source.calculated.assays << 'Expression array'
-  if (ctx._source.assays.exomeseq) ctx._source.calculated.assays << 'Exome-seq'
-  if (ctx._source.assays.rnaseq) ctx._source.calculated.assays << 'RNA-seq'
-  if (ctx._source.assays.mtarray) ctx._source.calculated.assays << 'Methylation array'
-  if (ctx._source.assays.proteomics) ctx._source.calculated.assays << 'Proteomics'
-  if (ctx._source.assays['cellbiol-fn']) ctx._source.calculated.assays << 'Cellular phenotyping'
+  if (ctx._source.assays.gtarray) {
+      ctx._source.calculated.assays << 'Genotyping array'
+      ctx._source.searchable.free << 'genotyping'
+  }
+  if (ctx._source.assays.gexarray) {
+      ctx._source.calculated.assays << 'Expression array'
+      ctx._source.searchable.free << 'expression'
+  }
+  if (ctx._source.assays.exomeseq) {
+      ctx._source.calculated.assays << 'Exome-seq'
+      ctx._source.searchable.free << 'exome'
+  }
+  if (ctx._source.assays.rnaseq) {
+      ctx._source.calculated.assays << 'RNA-seq'
+      ctx._source.searchable.fixed << 'RNA-seq'
+      ctx._source.searchable.free << 'rnaseq'
+  }
+  if (ctx._source.assays.mtarray) {
+      ctx._source.calculated.assays << 'Methylation array'
+      ctx._source.searchable.free << 'methylation'
+  }
+  if (ctx._source.assays.proteomics) {
+      ctx._source.calculated.assays << 'Proteomics'
+      ctx._source.searchable.free << 'proteomics'
+  }
+  if (ctx._source.assays['cellbiol-fn']) {
+      ctx._source.calculated.assays << 'Cellular phenotyping'
+  }
 }
 
 ctx._source.calculated.access = ctx._source.openAccess ? 'Open access' : ctx._source.openAccess != null ? 'Managed access' : ''
@@ -20,8 +44,6 @@ if (ctx._source.bankingStatus) {
   ctx._source.calculated.bankingStatus = ctx._source.bankingStatus.grep{!(it =~ /(?i)shipped/).find()}
 }
 
-ctx._source.searchable = [:]
-ctx._source.searchable.fixed = []
 
 if (ctx._source.name) {
     String name = ctx._source.name
