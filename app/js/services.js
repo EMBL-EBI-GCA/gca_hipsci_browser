3146d5951cfd7cca91d0a1b6c64ce37bc6f63eae
streeter <streeter@ebi.ac.uk>
2016-02-16 15:10:21 +0000

    http api gets should point to relative path, not full path

--- app/js/services.js
+++ app/js/services.js
@@ -9,14 +9,14 @@ services.service('apiClient', ['$http', function($http) {
         getSource: function (searchObj) {
             var type = searchObj.hasOwnProperty('type') ? searchObj.type : '';
             var id = searchObj.id;
-            var url = 'http://test.hipsci.org/lines/api/';
+            var url = 'api/';
             url = url.concat(type, '/', id);
             return $http.get(url, {cache: true});
         },
         search: function (searchObj) {
             var type = searchObj.hasOwnProperty('type') ? searchObj.type : '';
             var body = searchObj.hasOwnProperty('body') ? searchObj.body : {};
-            var url = 'http://test.hipsci.org/lines/api/';
+            var url = 'api/';
             url = url.concat(type, '/_search');
             return $http.post(url, body);
         },
@@ -26,7 +26,7 @@ services.service('apiClient', ['$http', function($http) {
             var body = searchObj.hasOwnProperty('body') ? searchObj.body : {};
             var format = searchObj.hasOwnProperty('format') ? searchObj.format : 'tsv';
             var filename = searchObj.hasOwnProperty('filename') ? searchObj.filename : type;
-            var url = 'http://test.hipsci.org/lines/api/';
+            var url = 'api/';
             url = url.concat(type, '/_search/', filename, '.', format);
             form.action= url;
             form.method='POST';
