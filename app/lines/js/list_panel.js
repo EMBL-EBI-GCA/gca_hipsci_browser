9cea3403f2654624a5c57510b5d8cf4a594f6bb4
streeter <streeter@ebi.ac.uk>
2017-02-16 14:36:13 +0000

    Fixed a memory leak by add destroy method

--- app/lines/js/list_panel.js
+++ app/lines/js/list_panel.js
@@ -97,6 +97,11 @@ listPanelModule.directive('listPanel', ['apiClient', '$location', function (apiC
             c.unbindRouteUpdate = null;
         }
       }
+      $scope.$on('$detroy', function() {
+        if (c.unbindRouteUpdate) {
+          c.unbindRouteUpdate();
+        }
+      });
 
       c.loadFromUrl = function(firstView, routeChangeFn) {
           c.routeUpdateUnlisten();
