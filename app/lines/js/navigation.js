47579d1dd45a41f105c19d53c481212e99bd2dc2
streeter <streeter@ebi.ac.uk>
2017-03-14 16:45:23 +0000

    More functionality and styling on the new header

--- app/lines/js/navigation.js
+++ app/lines/js/navigation.js
@@ -2,17 +2,23 @@
 
 var navigation = angular.module('hipsciBrowser.navigation', []);
 
-navigation.directive('navigationBanner', ['$location', function($location) {
+navigation.directive('navigationBanner', [function() {
     var activeLink = function (viewLocation) {
         return viewLocation == $location.path();
     };
 
     return {
         restrict: 'E',
-        templateUrl : 'partials/navigation.html?ver=20170308',
+        templateUrl : 'partials/navigation.html?ver=20170314',
         scope : true,
         link : function (scope) {
-            scope.activeLink = activeLink;
+            scope.collapsed = {};
+            scope.isCollapsed = function(label) {
+              return !scope.collapsed.hasOwnProperty(label) ? true : scope.collapsed[label];
+            }
+            scope.toggleCollapse = function(label) {
+              scope.collapsed[label] = !scope.isCollapsed(label);
+            }
         }
     };
 }]);
