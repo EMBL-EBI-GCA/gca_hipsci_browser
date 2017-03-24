ef86ffb6577f76a1a7c50dd9685bcc022ace628c
streeter <streeter@ebi.ac.uk>
2017-03-24 17:34:46 +0000

    Search page now lives inside the data portal

--- app/lines/js/controllers.js
+++ app/lines/js/controllers.js
@@ -560,3 +560,55 @@ controllers.controller('AssayDetailCtrl', ['$routeParams', '$http', 'lineTableVa
       });
   }
 ]);
+
+controllers.controller('SearchCtrl', ['$location', '$http', '$scope', 'apiClient',
+  function($location, $http, $scope, apiClient) {
+    var c = this;
+    $scope.siteHits = [];
+    $scope.lineHits = [];
+
+    $scope.q = $location.search().q;
+    if ($scope.q) {
+
+      var postBody1 = {
+        _source: ['url', 'title'],
+        query: {
+          match_phrase: {
+            content: $scope.q
+          }
+        },
+        highlight: {
+          fields: {
+            content: {}
+          },
+          pre_tags: ['<em><strong>'],
+          post_tags: ['</strong></em>'],
+        }
+      };
+      apiClient.search({
+        type: 'sitemap',
+        body: postBody1
+      }).then(function(resp) {
+        $scope.siteHits = resp.data.hits.hits;
+      });
+
+      var postBody2 = {
+        query: {
+          multi_match: {
+            query: $scope.q,
+            fields: ['searchable.free', 'searchable.fixed^3'],
+            type: 'most_fields',
+          }
+        },
+      };
+      apiClient.search({
+        type: 'cellLine',
+        body: postBody2
+      }).then(function(resp) {
+        $scope.lineHits = resp.data.hits.hits;
+      });
+
+    };
+
+  }
+]);
