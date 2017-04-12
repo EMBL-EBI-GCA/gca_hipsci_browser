855dd2fd35f69fbc44641ef68a2ca3d0837a4d7b
streeter <streeter@ebi.ac.uk>
2017-04-12 11:52:38 +0100

    Changed dates ready for deployment

--- app/lines/js/app.js
+++ app/lines/js/app.js
@@ -33,7 +33,7 @@ hipsciBrowser.config(['$routeProvider',
       controller: 'DonorDetailCtrl'
     }).
     when('/lines', {
-      templateUrl: 'partials/line-list.html?ver=20170324',
+      templateUrl: 'partials/line-list.html?ver=20170412',
       controller: 'LineListCtrl',
       controllerAs: 'LineCtrl',
       reloadOnSearch: false,
