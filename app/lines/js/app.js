e0aff85f8a0cae8e14dc3dec108e15cc85f4bd93
streeter <streeter@ebi.ac.uk>
2017-05-24 11:53:35 +0100

    small changes to idr section based on feedback

--- app/lines/js/app.js
+++ app/lines/js/app.js
@@ -19,12 +19,12 @@ hipsciBrowser.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
     when('/lines/:ipscName', {
-      templateUrl: 'partials/line-detail.html?ver=20170523',
+      templateUrl: 'partials/line-detail.html?ver=20170524',
       controller: 'LineDetailCtrl',
       controllerAs: 'LineCtrl',
     }).
     when('/lines/:ipscName/:assay', {
-      templateUrl: 'partials/line-assay-detail.html?ver=20170523',
+      templateUrl: 'partials/line-assay-detail.html?ver=20170524',
       controller: 'LineAssayCtrl',
       controllerAs: 'LineCtrl',
     }).
