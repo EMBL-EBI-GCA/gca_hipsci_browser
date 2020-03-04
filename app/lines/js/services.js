f40184855bdc43ceecd04d8dcbf82d861e4d8e92
apax18 <amirp@ebi.ac.uk>
2020-03-04 14:58:44 +0000

    console log

--- app/lines/js/services.js
+++ app/lines/js/services.js
@@ -140,7 +140,6 @@ services.factory('lineTableVars', function lineTableVarsFactory() {
               : field.esName == 'ecaccCatalogNumber' ? '<th class="purchase-button"></th>'
               : '<th>'+field.label+'</th>'
             var hitStr = 'hit['+i+']';
-            if (field.esName == 'assays.name') {console.log('+hitStr+'[$index])};
             // console.log(hitStr[$index]);
             field.td =
                 field.esName == 'bankingStatus' ? '<td class="matrix-dot"><div class="matrix-dot-item" popover="{{'+hitStr+'.text}}" popover-trigger="mouseenter"><span ng-bind="'+hitStr+'.letter" ng-class="'+hitStr+'.classes"></span></div></td>'
@@ -150,6 +149,7 @@ services.factory('lineTableVars', function lineTableVarsFactory() {
               : field.esName == 'ecaccCatalogNumber' ? '<td class="purchase-button"><a ng-if="'+hitStr+'" class="btn btn-sm btn-primary" ng-href="{{'+hitStr+'}}" target="_blank"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Purchase</a></td>'
               : field.esName == 'diseaseStatus.value' ? '<td class="disease-status" ng-bind="'+hitStr+'"></td>'
               : '<td ng-bind="'+hitStr+'"></td>'
+            if (field.esName == 'assays.name') {console.log('+hitStr+'[$index])};
         }
     }
 
